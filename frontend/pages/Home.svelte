58faa03ff74d19a263d870a84f3290628f7012a2
Raszbanana <niko.hochloff@gmail.com>
2022-06-07 20:17:28 +0200

    test: pushing socket testing impl to desktop

--- frontend/pages/Home.svelte
+++ frontend/pages/Home.svelte
@@ -6,11 +6,13 @@
 
   const socket = io('http://localhost:3000');
   console.log(socket);
+  
   let lastPersonToChangeAColor;
-  let counter = 0;
+  let counter = 30;
+
   socket.on('changeTheColor', ({ data }) => {
     console.log(data)
-    counter++
+    counter = counter * 1.5
     document.body.style.backgroundColor = data;
     lastPersonToChangeAColor = username;
   });
@@ -32,12 +34,21 @@
   <h2>
     Last person to change the color: {lastPersonToChangeAColor || 'noone'}"
   </h2>
-  <div>{counter}</div>
+  <div class="test" style="height: {counter}px; width: {counter}px;">{counter}</div>
   <input type="color" on:change={changeColor} />
+  <button on:click={changeColor}>click me</button>
 </div>
 
 <style>
   h1 {
     font-size: 30px;
   }
+
+  .test {
+    justify-content: center;
+    display: flex;
+    margin-left: 50%;
+    background-color: red;
+  }
+
 </style>
