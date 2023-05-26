9d97fc2c99d723a95ba8f3b3c2aa7c393ddf0fb1
Paweł Stępień <pstepien@interia.pl>
2023-05-26 20:53:24 +0200

    fixed the upload picture

--- backend/multer/multer.js
+++ backend/multer/multer.js
@@ -1,22 +1,22 @@
-import multer from 'multer';
+import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, "uploads/");
   },
   filename(req, file, cb) {
-    const filenameParts = file.originalname.split('.');
+    const filenameParts = file.originalname.split(".");
     if (filenameParts.length <= 1) {
-      cb(new Error('File has invalid extension ' + file.originalname));
+      cb(new Error("File has invalid extension " + file.originalname));
       return;
     }
 
     const extension = filenameParts[filenameParts.length - 1];
-    const originalFileName = filenameParts.join('.');
+    const originalFileName = filenameParts.join(".");
     const uniqueSuffix = Date.now();
 
     const newFileName =
-      uniqueSuffix + '___' + originalFileName + '.' + extension;
+      uniqueSuffix + "___" + originalFileName + "." + extension;
 
     cb(null, newFileName);
   },
@@ -24,10 +24,10 @@ const storage = multer.diskStorage({
 
 const imageFileFilter = (req, file, cb) => {
   // Accept only image files
-  if (file.mimetype.startsWith('image/')) {
+  if (file.mimetype.startsWith("image/")) {
     cb(null, true); // Accept the file
   } else {
-    cb('You provided a non image filetype'); // Reject the file
+    cb("You provided a non image filetype"); // Reject the file
   }
 };
 
