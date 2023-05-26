eb3022e45070f4be6357f7f0193ae842f3b178f6
Raszbanana <niko.hochloff@gmail.com>
2023-05-26 18:58:27 +0200

    added limiter

--- backend/db/schema/validators/post-validator.schema.js
+++ backend/db/schema/validators/post-validator.schema.js
@@ -1,10 +1,9 @@
 import joi from 'joi';
 
 export const postValidator = joi.object({
-  title: joi.string().required(),
-  text: joi.string().required(),
-  timestamp: joi.date().required(),
-  comments: joi.array().items(joi.string()),
+  title: joi.string().max(150).required(),
+  text: joi.string().max(255).required(),
+  comments: joi.array().items(joi.string().max(255)),
   likes: joi.array().items(joi.string()),
   postedBy: joi.string().required(),
 });
