d0f0db98c4d720fe00b5ecb71ebfb50a34dcee1e
Paweł Stępień <pstepien@interia.pl>
2023-05-26 20:21:25 +0200

    working on the upload picutres

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import path from 'path';
-import cors from 'cors';
-import helmet from 'helmet';
-import rateLimit from 'express-rate-limit';
+import express from "express";
+import path from "path";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
-import { resetHasClicked } from './schedules/hasClicked.js';
-import { connectToDB } from './db/connection/connect-to-db.js';
+import { resetHasClicked } from "./schedules/hasClicked.js";
+import { connectToDB } from "./db/connection/connect-to-db.js";
 
 const app = express();
 
@@ -13,10 +13,10 @@ const app = express();
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 30, // limit each IP to 100 requests per windowMs
-  message: 'Too many requests from this IP, please try again later',
+  message: "Too many requests from this IP, please try again later",
 });
 
-app.use(limiter);
+/* app.use(limiter); */
 
 // Setting content security policy to restrict execution of scripts to prevent XSS attacks
 app.use(
@@ -29,14 +29,14 @@ app.use(
 
 // Checking origin and referer headers to prevent CSRF attacks
 const checkOriginAndRefererHeaders = (req, res, next) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const origin = req.headers.origin;
     const referer = req.headers.referer;
 
-    if (origin && referer !== 'http://localhost:8080/') {
+    if (origin && referer !== "http://localhost:8080/") {
       return res
         .status(403)
-        .json({ error: 'Invalid Origin or Referer header' });
+        .json({ error: "Invalid Origin or Referer header" });
     }
   }
   next();
@@ -45,26 +45,27 @@ const checkOriginAndRefererHeaders = (req, res, next) => {
 app.use(checkOriginAndRefererHeaders);
 app.use(cors());
 
-app.use(express.static(path.resolve('../frontend/public')));
+app.use(express.static(path.resolve("../frontend/public")));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
-import userRouter from './routers/userRouter.js';
-app.use(userRouter);
 
-import postRouter from './routers/postRouter.js';
-app.use(postRouter);
-
-import authRouter from './routers/authRouter.js';
+import authRouter from "./routers/authRouter.js";
 app.use(authRouter);
 
-import beerRouter from './routers/beerRouter.js';
+import userRouter from "./routers/userRouter.js";
+app.use(userRouter);
+
+import postRouter from "./routers/postRouter.js";
+app.use(postRouter);
+
+import beerRouter from "./routers/beerRouter.js";
 app.use(beerRouter);
 
-import cockRouter from './routers/cocktailRouter.js';
+import cockRouter from "./routers/cocktailRouter.js";
 app.use(cockRouter);
 
-import emailRouter from './routers/emailRouter.js';
+import emailRouter from "./routers/emailRouter.js";
 app.use(emailRouter);
 
 // Resets hasClicked everyday at midnight European/Copenhagen
@@ -79,5 +80,5 @@ connectToDB()
     });
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB', error);
+    console.error("Error connecting to MongoDB", error);
   });
